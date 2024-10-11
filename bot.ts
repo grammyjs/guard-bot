87a7d79cbb0fc08d90590fc1a040a6a226cf4f72
KnorpelSenf <shtrog@gmail.com>
2024-10-11 19:25:35 +0200

    feat: switch to inline keyboards

--- bot.ts
+++ bot.ts
@@ -1,26 +1,49 @@
 import {
     Bot,
-    Keyboard,
+    type Context,
+    InlineKeyboard,
     webhookCallback,
 } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
 import { emoji } from "https://deno.land/x/grammy_emoji@v1.2.0/mod.ts";
 
+const isDebug = !!Deno.env.get("DEBUG");
 const kv = await Deno.openKv();
 
 const welcomeMessage =
-    "Hi! Glad to see that you want to join @grammyjs! Let me make sure that you are human. Which emojis do you see? Please use the buttons below!";
+    "Hi! Glad to see that you want to join @grammyjs! Let me make sure that you are human.";
+
+const empty = emoji("white_large_square");
+function inputMessage(first = empty, second = empty, third = empty) {
+    return `
+Which emojis do you see in the slot machine? Please enter them using the buttons below!
+
+${emoji("red_question_mark")}:  ${first}${second}${third}`;
+}
+function correctMessage(first: string, second: string, third: string) {
+    return `
+Correct! Welcome to @grammyjs, human!
+
+${emoji("check_mark_button")}:  ${first}${second}${third}`;
+}
+function incorrectMessage(first: string, second: string, third: string) {
+    return `
+Incorrect. Are you sure that you are human?
+
+${emoji("cross_mark")}:  ${first}${second}${third}`;
+}
+
 const helpTextPreRequest =
     "This bot protects the chat @grammyjs. You did not request to join it, so this bot does nothing for you right now.";
 const helpTextPostRequest = `
 You have requested to join @grammyjs. We are happy to welcome you to the chat as soon as you have confirmed that you are human.
 
-You can do this by sending me the three values that you see in the slot machine above. Simply tap three of the buttons beneath this message in the order that you see them.
+You can do this by sending me the three values that you see in the slot machine above. Simply tap three of the buttons above in the order that you see them.
 
-(If you are completely stuck and you have no idea how to solve this captcha, feel free to open an issue on GitHub. You can find the repository linked at the top of grammy.dev.)`;
+(If you are completely stuck and you have no idea how to solve this captcha, feel free to open an issue on GitHub and tell us your Telegram username so we can add you to the chat. You can find the repository linked at the top of grammy.dev.)`;
 const thirtyMinutesInMilliseconds = 30 * 60 * 1000;
 
 const em = [
-    "BAR",
+    emoji("minus"),
     emoji("grapes"),
     emoji("lemon"),
     emoji("keycap_digit_seven"),
@@ -33,7 +56,9 @@ function predictEmoji(value: number): [string, string, string] {
         em[(n & 0b110000) >> 4],
     ];
 }
-const keyboard = Keyboard.from([em.map(Keyboard.text)]).resized();
+const keyboard = InlineKeyboard.from([em.map((e) => InlineKeyboard.text(e))])
+    .toFlowed(2)
+    .row().text(emoji("back_arrow"), "back");
 
 const bot = new Bot(Deno.env.get("BOT_TOKEN") ?? "");
 const secretToken = bot.token.replaceAll(":", "_");
@@ -47,28 +72,29 @@ bot.on("my_chat_member")
 bot.on("chat_join_request", async (ctx) => {
     const dm = ctx.chatJoinRequest.user_chat_id;
     await ctx.api.sendMessage(dm, welcomeMessage);
-    const message = await ctx.api.sendDice(dm, emoji("slot_machine"), {
-        reply_markup: keyboard,
-    });
+    await sendCaptcha(ctx, dm);
+});
+async function sendCaptcha(ctx: Context, chatId: number) {
+    const message = await ctx.api.sendDice(chatId, emoji("slot_machine"));
+    await ctx.reply(inputMessage(), { reply_markup: keyboard });
     const value = message.dice.value;
-    const kv = await Deno.openKv();
-    await kv.set([dm, "solution"], value, {
+    await kv.set([chatId, "solution"], value, {
         expireIn: thirtyMinutesInMilliseconds,
     });
-});
+}
 // only respond in private chats
 const dm = bot.chatType("private");
 dm.command("help", async (ctx) => {
     const dm = ctx.chatId;
     const solution = await kv.get<number>([dm, "solution"]);
-    if (solution.value === null) {
-        await ctx.reply(helpTextPreRequest);
-    } else {
-        await ctx.reply(helpTextPostRequest, { reply_markup: keyboard });
-    }
+    await ctx.reply(
+        solution.value === null ? helpTextPreRequest : helpTextPostRequest,
+    );
 });
+dm.filter(() => isDebug).command("test", (ctx) => sendCaptcha(ctx, ctx.chatId));
 // disable bot for members and banned accounts
-const captcha = dm.on(":text").filter(async (ctx) => {
+const captcha = dm.filter(async (ctx) => {
+    if (isDebug) return true;
     const chatMember = await ctx.api.getChatMember("@grammyjs", ctx.from.id);
     switch (chatMember.status) {
         case "administrator":
@@ -85,40 +111,77 @@ const captcha = dm.on(":text").filter(async (ctx) => {
     }
     return true;
 });
-// handle emoji input from keyboard in DM
-captcha.hears(em, async (ctx) => {
+// handle emoji input from inline keyboard
+captcha.callbackQuery(em, async (ctx) => {
     const dm = ctx.chatId;
     const solution = await kv.get<number>([dm, "solution"]);
     if (solution.value === null) {
-        await ctx.reply(
-            "Please request to join @grammyjs before sending any emoji to me.",
+        await ctx.editMessageText(
+            "The captcha has expired. You need to request to join @grammyjs again.",
+            { reply_markup: undefined },
         );
         return;
     }
 
     const res = await kv.get<string[]>([dm, "input"]);
     const current = res.value ?? [];
-    current.push(ctx.msg.text);
+    current.push(ctx.callbackQuery.data);
     if (current.length >= 3) {
+        await ctx.editMessageText(inputMessage(...current), {
+            reply_markup: undefined,
+        });
         const [i0, i1, i2] = current;
         const [s0, s1, s2] = predictEmoji(solution.value);
         if (i0 === s0 && i1 === s1 && i2 === s2) {
-            await ctx.reply("Correct! Welcome to @grammyjs!", {
-                reply_markup: { remove_keyboard: true },
-            });
-            await ctx.api.approveChatJoinRequest("@grammyjs", ctx.from.id);
+            await ctx.editMessageText(correctMessage(i0, i1, i2));
+            if (!isDebug) {
+                await ctx.api.approveChatJoinRequest("@grammyjs", ctx.from.id);
+            }
         } else {
-            await ctx.reply(
-                "Incorrect. Are you sure that you are human? Please try again.",
-            );
+            await ctx.editMessageText(incorrectMessage(i0, i1, i2), {
+                reply_markup: new InlineKeyboard().text("Try again", "again"),
+            });
         }
         await kv.delete([dm, "input"]);
     } else {
-        await ctx.reply(`Current input: ${current.join(" ")}`);
+        await ctx.editMessageText(inputMessage(...current), {
+            reply_markup: keyboard,
+        });
         await kv.set([dm, "input"], current, {
             expireIn: thirtyMinutesInMilliseconds,
         });
     }
+});
+// handle back button
+captcha.callbackQuery("back", async (ctx) => {
+    const dm = ctx.chatId;
+    const solution = await kv.get<number>([dm, "solution"]);
+    if (solution.value === null) {
+        await ctx.editMessageText(
+            "The captcha has expired. You need to request to join @grammyjs again.",
+            { reply_markup: undefined },
+        );
+        return;
+    }
+
+    const res = await kv.get<string[]>([dm, "input"]);
+    const current = res.value ?? [];
+    if (current.length === 0) {
+        await ctx.answerCallbackQuery();
+        return;
+    }
+    current.pop();
+    await ctx.editMessageText(inputMessage(...current), {
+        reply_markup: keyboard,
+    });
+    await kv.set([dm, "input"], current, {
+        expireIn: thirtyMinutesInMilliseconds,
+    });
+});
+// handle try again button
+captcha.callbackQuery("again", async (ctx) => {
+    await ctx.editMessageReplyMarkup([]);
+    await sendCaptcha(ctx, ctx.chatId);
 });
 // handle any other updates
 captcha.use(async (ctx) => {
@@ -135,5 +198,5 @@ captcha.use(async (ctx) => {
     });
 });
 
-if (Deno.env.get("DEBUG")) bot.start();
+if (isDebug) bot.start();
 else Deno.serve(webhookCallback(bot, "std/http", { secretToken }));
